177a545665c56b34214e55cf26aa30b18d435546
Arun Gurung <arungurung@Aruns-MBP.local>
2019-06-01 12:04:50 +1000

    model ready

--- Processing/data_evaluation.py
+++ Processing/data_evaluation.py
@@ -1,5 +1,6 @@
 # %%
 
+from tensorflow.python.tools import freeze_graph
 import pandas as pd  # for data processing CSV data
 import tensorflow as tf
 import numpy as np  # for linear algebra
@@ -9,25 +10,28 @@ from scipy import stats
 import seaborn as sns
 from pylab import rcParams
 from sklearn import metrics
+from tensorflow.contrib import rnn
+# for splitting data to test and train
 from sklearn.model_selection import train_test_split
 
 sns.set(style='whitegrid', palette='muted', font_scale=1.5)
 
-rcParams['figure.figsize'] = 1, 1
+rcParams['figure.figsize'] = 14, 8
 
 RANDOM_SEED = 42
 
-columns = ['user', 'activity', 'x-axis', 'y-axis', 'z-axis']
+columns = ['user', 'activity', 'timestamp', 'x-axis', 'y-axis', 'z-axis']
 # dataframe
-df = pd.read_csv('data/reduced.txt',
+df = pd.read_csv('data/WISDM_ar_v1.1_raw.txt',
                  header=None, names=columns)
 df.head()
 
-# exploring the data
-df['activity'].value_counts().plot(
-    kind='bar', title='Training examples by activity type')
-df['user'].value_counts().plot(
-    kind='bar', title='Training examples by user')
+# %%
+# # exploring the data
+# df['activity'].value_counts().plot(
+#     kind='bar', title='Training examples by activity type')
+# df['user'].value_counts().plot(
+#     kind='bar', title='Training examples by user')
 
 # custom function to plot
 
@@ -41,7 +45,228 @@ def plot_activity(activity, df):
 
 
 # graph showing activity using function
-plot_activity("Sitting", df)
-plot_activity("Standing", df)
-plot_activity("Walking", df)
-plot_activity("Jogging", df)
+# plot_activity("Sitting", df)
+
+
+# %%
+# plot_activity("Walking", df)
+# plot_activity("Jogging", df)
+
+# %%
+# data preprocessing - reducing size
+N_TIME_STEPS = 200
+N_FEATURES = 3
+step = 100
+segments = []
+labels = []
+for i in range(0, len(df) - N_TIME_STEPS, step):
+    xs = df['x-axis'].values[i: i + N_TIME_STEPS]
+    ys = df['y-axis'].values[i: i + N_TIME_STEPS]
+    zs = df['z-axis'].values[i: i + N_TIME_STEPS]
+    label = stats.mode(df['activity'][i: i + N_TIME_STEPS])[0][0]
+    segments.append([xs, ys, zs])
+    labels.append(label)
+
+# %%
+
+# check data
+np.array(segments).shape
+
+# %%
+
+# transforming to sequences of 200 rows into three axis
+reshaped_segments = np.asarray(
+    segments, dtype=np.float32).reshape(-1, N_TIME_STEPS, N_FEATURES)
+labels = np.asarray(pd.get_dummies(labels), dtype=np.float32)
+
+# check data
+reshaped_segments.shape
+
+# %%
+
+# splitting data to training (80%) and test (20%)
+x_train, x_test, y_train, y_test = train_test_split(
+    reshaped_segments, labels, test_size=0.2, random_state=RANDOM_SEED
+)
+
+# %%
+
+# building the model
+N_CLASSES = 6
+N_HIDDEN_UNITS = 64
+
+
+def create_LSTM_model(inputs):
+    W = {
+        'hidden': tf.Variable(tf.random_normal([N_FEATURES, N_HIDDEN_UNITS])),
+        'output': tf.Variable(tf.random_normal([N_HIDDEN_UNITS, N_CLASSES]))
+    }
+    biases = {
+        'hidden': tf.Variable(tf.random_normal([N_HIDDEN_UNITS], mean=1.0)),
+        'output': tf.Variable(tf.random_normal([N_CLASSES]))
+    }
+
+    X = tf.transpose(inputs, [1, 0, 2])
+    X = tf.reshape(X, [-1, N_FEATURES])
+    hidden = tf.nn.relu(tf.matmul(X, W['hidden']) + biases['hidden'])
+    hidden = tf.split(hidden, N_TIME_STEPS, 0)
+
+    # Stack 2 LSTM layers
+    lstm_layers = [tf.contrib.rnn.BasicLSTMCell(
+        N_HIDDEN_UNITS, forget_bias=1.0) for _ in range(2)]
+    lstm_layers = tf.contrib.rnn.MultiRNNCell(lstm_layers)
+
+    outputs, _ = tf.contrib.rnn.static_rnn(
+        lstm_layers, hidden, dtype=tf.float32)
+
+    # Get output for the last time step
+    lstm_last_output = outputs[-1]
+
+    return tf.matmul(lstm_last_output, W['output'] + biases['output'])
+
+# %%
+
+
+# placeholder for the model
+tf.reset_default_graph()
+
+X = tf.placeholder(tf.float32, [None, N_TIME_STEPS, N_FEATURES], name="input")
+Y = tf.placeholder(tf.float32, [None, N_CLASSES])
+
+# %%
+
+# for input
+pred_Y = create_LSTM_model(X)
+
+pred_softmax = tf.nn.softmax(pred_Y, name="y_")
+
+# %%
+
+# using L2 regularization
+L2_LOSS = 0.0015
+l2 = L2_LOSS * \
+    sum(tf.nn.l2_loss(tf_var) for tf_var in tf.trainable_variables())
+
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+    logits=pred_Y, labels=Y)) + l2
+
+# %%
+# defining optimizer and accuracy optimizers
+LEARNING_RATE = 0.0025
+
+optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(loss)
+
+correct_pred = tf.equal(tf.argmax(pred_softmax, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, dtype=tf.float32))
+
+# %%
+
+# training
+N_EPOCHS = 50
+BATCH_SIZE = 1024
+
+saver = tf.train.Saver()
+
+history = dict(train_loss=[], train_acc=[], test_loss=[], test_acc=[])
+
+# session
+sess = tf.InteractiveSession()
+sess.run(tf.global_variables_initializer())
+
+train_count = len(x_train)
+
+for i in range(1, N_EPOCHS + 1):
+    for start, end in zip(range(0, train_count, BATCH_SIZE), range(BATCH_SIZE, train_count + 1, BATCH_SIZE)):
+        sess.run(optimizer, feed_dict={
+                 X: x_train[start:end], Y: y_train[start:end]})
+    _, acc_train, loss_train = sess.run([pred_softmax, accuracy, loss], feed_dict={
+        X: x_train, Y: y_train})
+
+    _, acc_test, loss_test = sess.run([pred_softmax, accuracy, loss], feed_dict={
+        X: x_test, Y: y_test})
+
+    history['train_loss'].append(loss_train)
+    history['train_acc'].append(acc_train)
+    history['test_loss'].append(loss_test)
+    history['test_acc'].append(acc_test)
+
+    if i != 1 and i % 10 != 0:
+        continue
+
+    print(f'epoch: {i} test accuracy: {acc_test} loss: {loss_test}')
+
+predictions, acc_final, loss_final = sess.run(
+    [pred_softmax, accuracy, loss], feed_dict={X: x_test, Y: y_test})
+
+print()
+print(f'final results: accuracy: {acc_final} loss: {loss_final}')
+
+# %%
+
+# loading model to disk
+pickle.dump(predictions, open("predictions.p", "wb"))
+pickle.dump(history, open("history.p", "wb"))
+tf.train.write_graph(sess.graph_def, '.' '/checkpoint/my-model', 'train.pbtxt')
+saver.save(sess, save_path="./checkpoint/my-model.ckpt")
+
+# %%
+# close session
+sess.close()
+
+# %%
+
+# reloading model
+history = pickle.load(open("history.p", "rb"))
+predictions = pickle.load(open("predictions.p", "rb"))
+
+# %%
+
+# evaluation
+plt.figure(figsize=(12, 8))
+plt.plot(np.array(history['train_loss']), "r--", label="Train loss")
+plt.plot(np.array(history['train_acc']), "g--", label="Train accuracy")
+plt.plot(np.array(history['test_loss']), "r-", label="Test loss")
+plt.plot(np.array(history['test_acc']), "g-", label="Test accuracy")
+plt.title("Training session's progress over iterations")
+plt.legend(loc='upper right', shadow=True)
+plt.ylabel('Training Progress (Loss or Accuracy values)')
+plt.xlabel('Training Epoch')
+plt.ylim(0)
+plt.show()
+
+# %%
+
+# confusion matrix
+LABELS = ['Downstairs', 'Jogging', 'Sitting',
+          'Standing', 'Upstairs', 'Walking']
+max_test = np.argmax(y_test, axis=1)
+max_predictions = np.argmax(predictions, axis=1)
+confusion_matrix = metrics.confusion_matrix(max_test, max_predictions)
+plt.figure(figsize=(16, 14))
+sns.heatmap(confusion_matrix, xticklabels=LABELS,
+            yticklabels=LABELS, annot=True, fmt="d")
+plt.title("Confusion matrix")
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.show()
+
+# %%
+
+# exporting model
+
+MODEL_NAME = 'my-model'
+
+input_graph_path = 'checkpoint/my-model/train.pbtxt'
+checkpoint_path = './checkpoint/' + MODEL_NAME+'.ckpt'
+restore_op_name = "save/restore_all"
+filename_tensor_name = "save/Const:0"
+output_frozen_graph_name = 'frozen_'+MODEL_NAME+'.pb'
+
+freeze_graph.freeze_graph(input_graph_path, input_saver="",
+                          input_binary=False, input_checkpoint=checkpoint_path,
+                          output_node_names="y_", restore_op_name="save/restore_all",
+                          filename_tensor_name="save/Const:0",
+                          output_graph=output_frozen_graph_name, clear_devices=True, initializer_nodes="")
+
+
+# %%
